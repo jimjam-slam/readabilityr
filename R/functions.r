bfa4116e85a753e065c26771526ea07cb7a0f37e
James Goldie <james@dicenerd.app>
2024-10-21 15:20:56 +1100

    WIP package API stubs
    Co-authored-by: Chitra M Saraswati <chitrams@users.noreply.github.com>
    Co-authored-by: Janith Wanniarachchi <janithcwanni@gmail.com>
    Co-authored-by: Jayani Lakshika <JayaniLakshika@users.noreply.github.com>

---
+++ R/functions.r
@@ -0,0 +1,11 @@
+can_read <- function (document, min_content_length = 140, min_score = 20) {
+
+    # An R function that wraps around the isProbablyReaderable function
+
+}
+
+parse_site <- function () {
+
+    # An R function that parses the website
+
+}
